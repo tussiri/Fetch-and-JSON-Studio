69319d1561b777acece9dc8b1b971a7f15354d9d
Your Name <you@example.com>
2022-11-28 15:31:06 -0600

    could not fix the spacing of the skills

--- script.js
+++ script.js
@@ -3,26 +3,26 @@
 window.addEventListener("load", function () {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
         response.json().then(function (json) {
-            console.log(json[0].firstName);
+            // console.log(json[0].firstName);
             let container = document.getElementById('container')
-            for (var astronaut of json){
-                console.log(astronaut.firstName)
+            for (let astronaut in json){
+                // console.log(astronaut.firstName)
             container.innerHTML += `
     <div class ="astronaut">
         <div class="bio">
-            <h3>Name: ${astronaut.firstName}<h3>
+            <h3>Name: ${json[astronaut].firstName} ${json[astronaut].lastName}</h3>
                 <ul>
-                    <li>Hours in Space: ${astronaut.hoursInSpace}</li>
-                    <li>Active: ${astronaut.active}</li>
-                    <li>Skills: ${astronaut.skills}</li>
+                    <li>Hours in Space: ${json[astronaut].hoursInSpace}</li>
+                    <li>Active: ${json[astronaut].active}</li>
+                    <li>Skills: ${json[astronaut].skills}</li>
                 </ul>
         </div>
-          <img class="avatar" src=${astronaut.picture}>
+          <img class="avatar" src=${json[astronaut].picture}>
     </div>`
         }
-        console.log(container.innerHTML)
-        console.log(typeof container.innerHTML)
+        // console.log(container.innerHTML)
+        // console.log(typeof container.innerHTML)
     });
-    console.log('window loaded')
+    // console.log('window loaded')
     });
 })
