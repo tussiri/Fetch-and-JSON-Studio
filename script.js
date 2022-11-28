fd7e431c464e7608feba9b1b996d38f3f18feee4
Your Name <you@example.com>
2022-11-28 16:52:55 -0600

    finished studio, still haven't figured out the spacing, and the Active: true for the first astronaut

--- script.js
+++ script.js
@@ -4,25 +4,35 @@ window.addEventListener("load", function () {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
         response.json().then(function (json) {
             // console.log(json[0].firstName);
-            let container = document.getElementById('container')
-            for (let astronaut in json){
-                // console.log(astronaut.firstName)
-            container.innerHTML += `
-    <div class ="astronaut">
-        <div class="bio">
-            <h3>Name: ${json[astronaut].firstName} ${json[astronaut].lastName}</h3>
-                <ul>
-                    <li>Hours in Space: ${json[astronaut].hoursInSpace}</li>
-                    <li>Active: ${json[astronaut].active}</li>
-                    <li>Skills: ${json[astronaut].skills}</li>
-                </ul>
-        </div>
-          <img class="avatar" src=${json[astronaut].picture}>
-    </div>`
-        }
-        // console.log(container.innerHTML)
-        // console.log(typeof container.innerHTML)
-    });
-    // console.log('window loaded')
+            const container = document.getElementById('container')
+            container.innerHTML += `<p> Total Numnber of Astronauts: ${json.length}</p>
+            `;
+            for (let astronaut in json) {
+                for (let i = 0; i < json.length; i++) {
+                if (json[astronaut].active) {
+                    activeColor = 'style="color:green"';
+                }else{
+                    activeColor = 'style="color:red"'
+                }
+            }
+                json.sort((a, b) => a.hoursInSpace - b.hoursInSpace);
+                container.innerHTML += `
+                    <div class ="astronaut">
+                        <div class="bio">
+                            <h3>Name: ${json[astronaut].firstName} ${json[astronaut].lastName}</h3>
+                                <ul id="astro">
+                                    <li>Hours in Space: ${json[astronaut].hoursInSpace}</li>
+                                    <li ${activeColor}>Active: ${json[astronaut].active}</li>
+                                    <li>Skills: ${json[astronaut].skills}</li>
+                                </ul>
+                        </div>
+                        <img class="avatar" src=${json[astronaut].picture}>
+                    </div>`
+            }
+
+            // console.log(container.innerHTML)
+            // console.log(typeof container.innerHTML)
+        });
+        // console.log('window loaded')
     });
 })
