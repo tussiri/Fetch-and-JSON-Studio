6d6c6043592ff1c45ad761ee0c5c69debe5e80a1
Your Name <you@example.com>
2022-11-28 20:19:37 -0600

    completed studio

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-// TODO: add code here
+// // TODO: add code here
 
 window.addEventListener("load", function () {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
-        response.json().then(function (json) {
-            // console.log(json[0].firstName);
+        response.json().then(function (astroData) {
+            console.log(astroData[0].firstName);
             const container = document.getElementById('container')
-            container.innerHTML += `<p> Total Numnber of Astronauts: ${json.length}</p>
+            container.innerHTML += `<p> Total Numnber of Astronauts: ${astroData.length}</p>
             `;
-            for (let astronaut in json) {
-                for (let i = 0; i < json.length; i++) {
-                if (json[astronaut].active) {
+            for (let astronaut in astroData) {
+                astroData.sort((a, b) => b.hoursInSpace - a.hoursInSpace);
+                for (let i = 0; i < astroData.length; i++) {
+                if (astroData[astronaut].active===true) {
                     activeColor = 'style="color:green"';
                 }else{
-                    activeColor = 'style="color:red"'
+                    activeColor = 'style="color:black"'
                 }
             }
-                json.sort((a, b) => a.hoursInSpace - b.hoursInSpace);
+                
                 container.innerHTML += `
                     <div class ="astronaut">
                         <div class="bio">
-                            <h3>Name: ${json[astronaut].firstName} ${json[astronaut].lastName}</h3>
+                            <h3>Name: ${astroData[astronaut].firstName} ${astroData[astronaut].lastName}</h3>
                                 <ul id="astro">
-                                    <li>Hours in Space: ${json[astronaut].hoursInSpace}</li>
-                                    <li ${activeColor}>Active: ${json[astronaut].active}</li>
-                                    <li>Skills: ${json[astronaut].skills}</li>
+                                    <li>Hours in Space: ${astroData[astronaut].hoursInSpace}</li>
+                                    <li ${activeColor}>Active: ${astroData[astronaut].active}</li>
+                                    <li>Skills: ${astroData[astronaut].skills.join(", ")}</li>
                                 </ul>
                         </div>
-                        <img class="avatar" src=${json[astronaut].picture}>
+                        <img class="avatar" src=${astroData[astronaut].picture}>
                     </div>`
             }
 
